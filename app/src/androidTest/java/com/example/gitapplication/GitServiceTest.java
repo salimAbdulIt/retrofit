addc3e149cdb765f908e54a3eb543805a3ba4800
salimAbdulIt <salimabdul.it@gmail.com>
2019-02-11 11:30:38 +0200

    initial commit

---
+++ app/src/androidTest/java/com/example/gitapplication/GitServiceTest.java
@@ -0,0 +1,14 @@
+package com.example.gitapplication;
+
+import com.example.gitapplication.service.GitService;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GitServiceTest {
+
+    @Test
+    public void getAllRepositories() throws Exception {
+    }
+}
---
+++ app/src/main/java/com/example/gitapplication/entity/Repository.java
@@ -0,0 +1,18 @@
+package com.example.gitapplication.entity;
+
+public class Repository
+{
+    private String name;
+
+
+    public Repository() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
